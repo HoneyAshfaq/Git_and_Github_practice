943a58587a84382fa26620120ec2b479d29c4a31
Honey <honeyashfaq3gmail.com>
2023-11-30 17:05:07 +0530

    6thcommit

--- src/test/java/kk/NewTest.java
+++ src/test/java/kk/NewTest.java
@@ -1,3 +1,7 @@
 // do some change
 // do some change
+<<<<<<< HEAD
 // do some change  vv// do some change // do some change// do some change // do some chan
+=======
+// do some change  // do some change // do some change// do some change 
+>>>>>>> 68b53be5c86e4eb585086d24309a4876b285f15f
